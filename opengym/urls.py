503d6f40bf0e860c653bed000bc97647eb68bbdc
mostaard <jonas@mostaard.be>
2022-09-05 13:55:23 +0200

    Fix: update loginview imports for django 2.2

--- opengym/urls.py
+++ opengym/urls.py
@@ -25,11 +25,11 @@ urlpatterns = [
     url(r'^', include('coursemanaging.urls')),
     url(r'^', include('mostaardimgur.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
-    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^password_reset/$', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        auth_views.password_reset_confirm, name='password_reset_confirm'),
-    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
+        auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_complete'),
 
 ]
 
