82d777bdb549d1a671d8e5020c46a195d5c74888
mostaard <jonas@mostaard.be>
2022-10-06 10:12:44 +0200

    Fix: update loginview to use template_name correctly

--- coursemanaging/urls.py
+++ coursemanaging/urls.py
@@ -46,7 +46,7 @@ urlpatterns = [
         name='user-detail'),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.activate,
         name='user-activate'),
-    url(r'^login/$', auth_views.LoginView.as_view(), {'template_name': 'coursemanaging/user-login.html'},
+    url(r'^login/$', auth_views.LoginView.as_view(template_name= 'coursemanaging/user-login.html'),
         name='login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(), {'next_page': '/'},
         name='logout'),
